91f4cf54640489ee395147a9ebc8c0828fce0d94
박동현 <jinee9523@gmail.com>
2025-08-27 02:04:33 +0900

    remove: 미사용 코드 삭제

--- domains/patternClassifier/application/port/pattern_classifier_port.go
+++
@@ -1,8 +0,0 @@
-package port
-
-import "context"
-
-type PatternClassifierPort interface {
-	GetIdToCategoryMap(cxt context.Context) (map[string]string, error)
-	GetCategoryToIdMap(cxt context.Context) (map[string]string, error)
-}
